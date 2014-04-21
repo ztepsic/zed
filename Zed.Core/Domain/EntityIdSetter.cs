f453cd266df9a6d823b2d20cd89f88405a4c6b87
Zeljko Tepsic <ztepsic@gmail.com>
2014-04-21 12:54:33 +0200

    Changed SetIdTo extension method to fluid interface

--- Zed.Core/Domain/EntityIdSetter.cs
+++ Zed.Core/Domain/EntityIdSetter.cs
@@ -34,8 +34,9 @@ namespace Zed.Core.Domain {
         /// <typeparam name="TId">Identifier (Id) type</typeparam>
         /// <param name="entity">entity instance</param>
         /// <param name="id">identifier (Id) value</param>
-        public static void SetIdTo<TId>(this Entity<TId> entity, TId id) {
+        public static Entity<TId> SetIdTo<TId>(this Entity<TId> entity, TId id) {
             SetIdOf(entity, id);
+            return entity;
         }
 
 
