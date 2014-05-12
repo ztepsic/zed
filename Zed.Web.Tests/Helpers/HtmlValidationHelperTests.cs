d1b3d9c5ea8c661fa1a3f67b3cb344c23d5bb909
Zeljko Tepsic <ztepsic@gmail.com>
2014-05-13 00:41:04 +0200

    Created Asp.Net MVC validation helpers
    Created Asp.NET MVC validation helper for ModelState.
    It checks if particular property is a valid field through lambda expression.

---
+++ Zed.Web.Tests/Helpers/HtmlValidationHelperTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Zed.Web.Tests.Helpers {
+    [TestFixture]
+    class HtmlValidationHelperTests {
+
+        [Ignore("How to test MVC helpers")]
+        [Test]
+        public void HasValidationErrorFor_ValidAndInvalidModel_IndicatorTrueOrFalse() {
+            // Arrange
+
+            // Act
+            
+
+            // Assert
+        }
+    }
+}
---
+++ Zed.Web/Helpers/HtmlValidationHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using Zed.Web.Extensions;
+
+namespace Zed.Web.Helpers {
+    /// <summary>
+    /// HTML validation helper/extension methods
+    /// </summary>
+    public static class HtmlValidationHelper {
+
+        /// <summary>
+        /// Checks the ModelState for an error, and returns true if error exists, otherwise false
+        /// </summary>
+        /// <typeparam name="TModel">The type of the model.</typeparam>
+        /// <param name="htmlHelper"></param>
+        /// <param name="expression">The expression tree representing a property to check if it is a valid field for the current model.</param>
+        /// <returns>True if error exists, otherwise false.</returns>
+        public static bool HasValidationErrorFor<TModel>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, object>> expression) {
+            FormContext formContext = htmlHelper.ViewContext.FormContext;
+            if (formContext == null) return false;
+            return htmlHelper.ViewData.ModelState.IsValidField(expression);
+        }
+    }
+}
