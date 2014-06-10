15642b5d43b3c026a8607edb2bbaab9713585093
Zeljko Tepsic <ztepsic@gmail.com>
2014-06-10 22:23:22 +0200

    Added nuget specifications

---
+++ Zed.Web_build_nuget.bat
@@ -0,0 +1,1 @@
+nuget pack Zed.Web/Zed.Web.csproj -IncludeReferencedProjects -Prop Configuration=Release -Symbols
---
+++ build_nuget_all.bat
@@ -0,0 +1,4 @@
+call Zed.Core_build_nuget.bat
+call Zed.Web_build_nuget.bat
+call Zed.NHibernate_build_nuget.bat
+call Zed.NHibernate.Web_build_nuget.bat
