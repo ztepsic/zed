05d47b0c6421bac946acd17ed3a53cde3f570bf1
Zeljko Tepsic <ztepsic@gmail.com>
2014-08-06 00:03:39 +0200

    Added a new route case for matching route

--- Zed.Web.Tests/Test/RouteTests.cs
+++ Zed.Web.Tests/Test/RouteTests.cs
@@ -14,6 +14,12 @@ namespace Zed.Web.Tests.Test {
             //RouteConfig.RegisterRoutes(routes);
 
             routes.MapRoute(
+               name: "TestExample",
+               url: "test/{action}/{id}",
+               defaults: new { controller = "TestController", action = "Index", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
                name: "Default",
                url: "{controller}/{action}/{id}",
                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
@@ -21,10 +27,12 @@ namespace Zed.Web.Tests.Test {
 
             const string url1 = "~/";
             const string url2 = "~/testController/testAction/111";
+            const string url3 = "~/test/";
 
             // Assert
             Assert.IsTrue(RouteTest.RouteMatch(url1, routes, "Index", "Home"));
             Assert.IsTrue(RouteTest.RouteMatch(url2, routes, "testAction", "testController", new { id = 111 }));
+            Assert.IsTrue(RouteTest.RouteMatch(url3, routes, "Index", "testController"));
 
         }
 
@@ -40,10 +48,10 @@ namespace Zed.Web.Tests.Test {
                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
            );
 
-            const string url = "~/testController/testAction/111/test-param";
+            const string url1 = "~/testController/testAction/111/test-param";
 
             // Assert
-            Assert.IsTrue(RouteTest.RouteNotMatch(url, routes));
+            Assert.IsTrue(RouteTest.RouteNotMatch(url1, routes));
             
         }
 
