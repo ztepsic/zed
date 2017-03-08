dc7c3a162ad0d8b1709d6f2fe04b0eeffa59135e
Zeljko Tepsic <ztepsic@gmail.com>
2017-03-08 01:15:41 +0100

    Added enum extensions which allows to get enum description from DecriptionAttribute

---
+++ Zed.Tests/Extensions/EnumExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Zed.Extensions;
+
+namespace Zed.Tests.Extensions {
+
+    public enum TestEnum {
+        [System.ComponentModel.Description("Number one")]
+        One,
+        [System.ComponentModel.Description("Number two")]
+        Two,
+        [System.ComponentModel.Description("Number three")]
+        Three,
+    }
+
+    [TestFixture]
+    public class EnumExtensionsTests {
+        [Test]
+        public void GetDescription_SomeEnumType_EnumDescription() {
+            // Arrange
+
+            // Act
+            var description = TestEnum.Two.GetEnumDescription();
+
+            // Assert
+            Assert.AreEqual("Number two", description);
+        }
+
+    }
+}
---
+++ Zed/Extensions/EnumExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Zed.Extensions {
+    /// <summary>
+    /// Enum extensions
+    /// </summary>
+    public static class EnumExtensions {
+
+        /// <summary>
+        /// Gets a description of enum
+        /// </summary>
+        /// <typeparam name="T">Enum type</typeparam>
+        /// <param name="enumValue">Current enum</param>
+        /// <returns>Enum description</returns>
+        public static string GetEnumDescription<T>(this T enumValue) where T : struct {
+            var type = enumValue.GetType();
+            if (!type.IsEnum) {
+                throw new ArgumentException($"{nameof(enumValue)} must be of Enum type", nameof(enumValue));
+            }
+
+            string description = enumValue.ToString();
+
+            var memberInfo = type.GetMember(enumValue.ToString());
+            if (memberInfo.Length > 0) {
+                var attributes = memberInfo[0].GetCustomAttributes(typeof (DescriptionAttribute), false);
+                if (attributes.Length > 0) {
+                    var descriptionAttribute = attributes[0] as DescriptionAttribute;
+                    if (descriptionAttribute != null)
+                        description = descriptionAttribute.Description;
+                }
+            }
+
+            return description;
+        }
+
+    }
+}
